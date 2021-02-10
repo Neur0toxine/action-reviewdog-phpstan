85b5047be86683bf899449a4ae8438a39d86c9b1
Neur0toxine <pashok9825@gmail.com>
2021-02-10 21:29:58 +0300

    format entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,5 +7,6 @@ fi
 
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
-php /usr/local/bin/phpstan.phar analyse ${INPUT_TARGET_DIRECTORY} --error-format=raw ${INPUT_ARGS} | uniq -u | \
-    | reviewdog -name=PHPStan -f=phpstan -reporter=${INPUT_REPORTER} -level=${INPUT_LEVEL} -diff='git diff'
+php /usr/local/bin/phpstan.phar analyse ${INPUT_TARGET_DIRECTORY} --no-progress --error-format=raw ${INPUT_ARGS} | \
+    uniq -u | \
+    reviewdog -name=PHPStan -f=phpstan -reporter=${INPUT_REPORTER} -level=${INPUT_LEVEL} -diff='git diff'
