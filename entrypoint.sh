9d9a11bbeeef79d77a52e8655ae17b8416e0b72d
Neur0toxine <pashok9825@gmail.com>
2021-02-10 21:39:09 +0300

    different uniq param because -u removes everything

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,5 +8,5 @@ fi
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
 php /usr/local/bin/phpstan.phar analyse ${INPUT_TARGET_DIRECTORY} --no-progress --error-format=raw ${INPUT_ARGS} | \
-    uniq -u | \
+    uniq -c | \
     reviewdog -name=PHPStan -f=phpstan -reporter=${INPUT_REPORTER} -level=${INPUT_LEVEL} -diff='git diff'
